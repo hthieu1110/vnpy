1c1df8097107d482fba22bb105422a72ea6655fa
Hieu. Ha Trong <hieu.ha@hsc.com.vn>
2025-10-16 14:54:52 +0700

    wip

--- cmd/vision.py
+++ cmd/vision.py
@@ -5,9 +5,12 @@ from vnpy.trader.engine import MainEngine
 from vnpy.trader.ui import MainWindow, create_qapp
 
 from vnpy_binance import BinanceSpotGateway
+
 # from vnpy_novastrategy import NovaStrategyApp
 from vnpy_ctastrategy import CtaStrategyApp
 from vnpy_chartwizard import ChartWizardApp
+from vnpy_paperaccount import PaperAccountApp
+
 
 def main():
     qapp = create_qapp()
@@ -16,10 +19,11 @@ def main():
     event_engine = EventEngine()
     main_engine = MainEngine(event_engine)
 
-    main_engine.add_gateway(BinanceSpotGateway, gateway_name="Vision")
-    main_engine.add_app(CtaStrategyApp)
-    main_engine.add_app(ChartWizardApp)
-    main_engine.add_app(CtaBacktesterApp)
+    # main_engine.add_gateway(BinanceSpotGateway, gateway_name="Vision")
+    main_engine.add_app(PaperAccountApp)
+    # main_engine.add_app(CtaStrategyApp)
+    # main_engine.add_app(ChartWizardApp)
+    # main_engine.add_app(CtaBacktesterApp)
 
     # main_engine.add_app(NovaStrategyApp)
 
@@ -28,6 +32,7 @@ def main():
     main_window.showMaximized()
 
     qapp.exec()
+
 
 if __name__ == "__main__":
     main()
